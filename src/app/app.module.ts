b4058abae07c43d2292bc50a8f31b23cee30b70a
Daryl Tadeo <djst.ebuenas@gmail.com>
2023-07-17 23:59:46 +0800

    Push Added Angular Material and Event Emitter Function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [ // Declarations of Component to be detectable by Angular
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [ // Importing of Modules needed to support our components
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent] // 3rd. Tells Angular to scan/render finally in the page the declared component or index.html
--- src/app/posts/post-create/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -6,11 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = 'NO CONTENT';
+  enteredTitle = '';
+  enteredContent = '';
+
+  // Event Emitter is a way to pass data or update other 
+  // components (p.s. must be a parent component of this one and
+  // the receiver should be also a child component of the
+  // parent component of this one.) when something change 
+  // in this specific component.
+  @Output() postCreated = new EventEmitter();
 
   // It's better to add "on" word on your events or functions.
   onAddPost() {
-    this.newPost = this.enteredValue;  
+    const post = { 
+      title: this.enteredTitle, 
+      content: this.enteredContent 
+    };
+
+    this.postCreated.emit(post);
   }
 }
