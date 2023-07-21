15f6a19eaa1d7ae50e70b8881f22039e1fda61e7
Daryl Tadeo <djst.ebuenas@gmail.com>
2023-07-21 21:10:05 +0800

    Completed Insert Fetch and Delete with MongoDB proceeding to enhancing application

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// Importing Schema / Model
+const Post = require('./models/post');
 
 const app = express();
 
-// This will return/parse valid JSON request to parse JSON data
+// Connecting to MongoDB
+mongoose.connect("mongodb+srv://Tadxss:TbdePGt9rlmS5Faf@meanstackcluster.g5vvi5p.mongodb.net/mean-stack?retryWrites=true").
+    then(() => {
+        console.log("Connected to MongoDB Database");
+    })
+    .catch(() => {
+        console.log("Connection Failed!");
+    });
+
+// This will return/parse valid JSON request to 
+// parse JSON data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -21,34 +35,41 @@ app.use((req, res, next) => {
     next();
 });
 
+// Inserting New Post
 app.post("/api/posts", (req, res, next) => {
-    const post = req.body;
-    console.log(post);
-
-    // Use 201 when adding new data to Database or Something
-    res.status(201).json({
-        message: "Post added successfully"
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    
+    post.save().then(createdPost => {
+        // Use 201 when adding new data to Database or Something
+        res.status(201).json({
+            message: "Post added successfully",
+            postId: createdPost._id
+        });
     });
 });
 
+// Fetching All Posts
 app.get("/api/posts", (req, res, next) => {
-    const posts = [
-        {
-            id: 'fadf12421l', 
-            title: 'First server-side post', 
-            content: 'This is coming from the server!'
-        },
-        {
-            id: 'gaqf18491k', 
-            title: 'Second server-side post', 
-            content: 'This is coming from the server!'
-        },
-    ];
+    Post.find()
+        .then(documents => {
+            res.status(200).json({
+                message: 'Posts fetched successfully!',
+                posts: documents
+            });
+        });
+});
 
-    res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts: posts
-    });
+// Deleting Post
+app.delete("/api/posts/:id", (req, res, next) => {
+    Post.deleteOne({ _id: req.params.id }).then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: "Post deleted!"
+            });
+        });
 });
 
 module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
-import { Subject } from 'rxjs';
+import { Subject, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,9 +17,20 @@ export class PostsService {
     // a best practice to just return it like this so that the
     // main array object is not being edited etc since we just
     // return a copy
-    this.http.get<{message: string, posts : Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http.get<{ message: string, posts: any }>(
+        'http://localhost:3000/api/posts'
+      )
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            id: post._id,
+            title: post.title,
+            content: post.content
+          }
+        });
+      }))
+      .subscribe(transformedPosts => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -32,11 +43,24 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post =  { id: null, title: title, content: content };
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    this.http.post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
       .subscribe((responseData) => {
-        console.log(responseData.message);
+        const id = responseData.postId;
+        post.id = id;
+
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
       });
+  }
+
+  deletePost(postId: string) {
+    this.http.delete('http://localhost:3000/api/posts/' + postId)
+      .subscribe(() => {
+        
+        // Updating lists of posts without fetching data again after deleting.
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      })
   }
 }
